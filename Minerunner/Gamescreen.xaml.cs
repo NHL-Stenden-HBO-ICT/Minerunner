aaacf577facb71bfc96bf12566598926a884b12a
L4rs <lars.ijntema@ziggo.nl>
2023-10-10 23:31:53 +0200

    Initial chunk testing

--- Minerunner/Gamescreen.xaml.cs
+++ Minerunner/Gamescreen.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Minerunner
 {
@@ -20,9 +21,85 @@ namespace Minerunner
     /// </summary>
     public partial class Gamescreen : Page
     {
+        private DispatcherTimer updateTimer = new DispatcherTimer();
+
+        private int[,] testChunk = {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 3, 0, 3, 0, 3, 3, 3, 0, 3, 0, 0, 0, 0, 0, 0 },
+            { 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0 },
+            { 0, 3, 3, 3, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0 },
+            { 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0 },
+            { 0, 3, 0, 3, 0, 3, 3, 3, 0, 3, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
+        };
+
         public Gamescreen()
         {
             InitializeComponent();
+
+            updateTimer.Interval = TimeSpan.FromMilliseconds(16);
+            updateTimer.Tick += OnUpdate;
+            updateTimer.Start();
+
+            RenderChunk(testChunk);
+        }
+
+        private Brush RandomColor()
+        {
+            Random r = new Random();
+            return new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255),
+                              (byte)r.Next(1, 255), (byte)r.Next(1, 233)));
+        }
+
+        private void RenderChunk(int[,] chunkData)
+        {
+            for (int i = 0; i < 256; i++)
+            {
+                int x = i % 16;
+                int y = i / 16;
+
+                Rectangle rect = new Rectangle();
+                rect.Width = rect.Height = 20;
+
+                switch (chunkData[y, x])
+                {
+                    case 0:
+                        rect.Fill = new SolidColorBrush(Color.FromRgb(49, 189, 175));
+                        break;
+
+                    case 1:
+                        rect.Fill = new SolidColorBrush(Color.FromRgb(49, 189, 79));
+                        break;
+
+                    case 2:
+                        rect.Fill = new SolidColorBrush(Color.FromRgb(94, 40, 16));
+                        break;
+
+                    case 3:
+                        rect.Fill = new SolidColorBrush(Color.FromRgb(222, 52, 235));
+                        break;
+                }
+
+                //rect.Fill = RandomColor();
+
+                ChunkCanvas.Children.Add(rect);
+                Canvas.SetLeft(rect, x * rect.Width);
+                Canvas.SetTop(rect, y * rect.Height);
+            }
+        }
+
+        private void OnUpdate(object? sender, EventArgs e)
+        {
         }
     }
 }
